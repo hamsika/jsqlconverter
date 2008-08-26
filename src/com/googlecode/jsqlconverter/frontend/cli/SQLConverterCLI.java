478444d2797555b49d30d6c124daf41ee254eda7
jamescarter <jamescarter@83d163da-d654-0410-8eb7-0be868dce84a>
2008-08-26 16:09:08 +0000

    Updated SQLConverterCLI to support output formats
    git-svn-id: https://jsqlconverter.googlecode.com/svn/trunk@12 83d163da-d654-0410-8eb7-0be868dce84a

--- src/com/googlecode/jsqlconverter/frontend/cli/SQLConverterCLI.java
+++ src/com/googlecode/jsqlconverter/frontend/cli/SQLConverterCLI.java
@@ -16,7 +16,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLConverterCLI {
-	private Operation op = null;
+	private InputOperation inputOp = null;
+	private OutputOperation outputOp = null;
 	private ArrayList<String> argList;
 
 	// jdbc params
@@ -27,30 +28,48 @@ public class SQLConverterCLI {
 
 	// delim / sql input
 	private String file;
+	private boolean hasHeaderRow = true;
+	private char delimiter = ',';
+	private char textQuantifier = '\0';
 
-	private enum Operation {
+	private enum InputOperation {
 		JDBC,
 		DELIMITED
+	}
+
+	private enum OutputOperation {
+		POSTGRESQL
 	}
 
 	public SQLConverterCLI(String[] args) throws ParserException, SQLException, FileNotFoundException, ClassNotFoundException {
 		if (args.length == 0) {
 			printUsage();
+			System.exit(0);
 		}
 
 		argList = new ArrayList<String>(Arrays.asList(args));
 
+		if (argList.contains("-h") || argList.contains("-help") || argList.contains("--help")) {
+			printUsage();
+			System.exit(0);
+		}
+
 		// detect operation
 		if (argList.contains("-jdbc")) {
-			setOperation(Operation.JDBC);
+			setInputOperation(InputOperation.JDBC);
 		}
 
 		if (argList.contains("-delim")) {
-			setOperation(Operation.DELIMITED);
+			setInputOperation(InputOperation.DELIMITED);
+		}
+
+
+		if (inputOp == null) {
+			printMessage("No input operation specified");
 		}
 
 		// detect parser options
-		switch(op) {
+		switch(inputOp) {
 			case JDBC:
 				driver = getRequiredParameter("-driver");
 				url = getRequiredParameter("-url");
@@ -59,11 +78,40 @@ public class SQLConverterCLI {
 			break;
 			case DELIMITED:
 				setStdInOrFile();
+
+				hasHeaderRow = !argList.contains("-noheader");
+
+				String seperator = getOptionalParameter("-seperator");
+
+				if (seperator != null) {
+					if (seperator.length() == 1) {
+						delimiter = seperator.charAt(0);
+					} else {
+						printMessage("Seperator must be a single character");
+					}
+				}
+
+
+				String quantifier = getOptionalParameter("-quantifier");
+
+				if (quantifier != null) {
+					if (quantifier.length() == 1) {
+						textQuantifier = quantifier.charAt(0);
+					} else {
+						printMessage("Text quantifier must be a single character");
+					}
+				}
 			break;
 		}
 
 		// detect producer options
+		if (argList.contains("-out-postgre")) {
+			setOutputOperation(OutputOperation.POSTGRESQL);
+		}
 
+		if (outputOp == null) {
+			printMessage("No output operation specified");
+		}
 
 		// run it! :)
 		run();
@@ -71,17 +119,13 @@ public class SQLConverterCLI {
 
 	private void setStdInOrFile() {
 		file = getOptionalParameter("-file");
-
-		if (file != null && argList.contains("-stdin")) {
-			throw new RuntimeException("Both stdin and file are defined");
-		}
 	}
 
 	public String getRequiredParameter(String param) {		
 		String value = getOptionalParameter(param);
 
 		if (value == null) {
-			throw new RuntimeException("Missing required parameter");
+			printMessage("Missing required parameter");
 		}
 
 		return value;
@@ -95,30 +139,38 @@ public class SQLConverterCLI {
 		int paramIndex = argList.indexOf(param);
 
 		if (paramIndex + 1 >= argList.size()) {
-			throw new RuntimeException("Parameter value missing for " + param);
+			printMessage("Parameter value missing for " + param);
 		}
 
 		return argList.get(paramIndex + 1);
 	}
 
-	private void setOperation(Operation newOp) {
-		if (op != null) {
-			throw new RuntimeException("Multiple operations found");
+	private void setInputOperation(InputOperation newOp) {
+		if (inputOp != null) {
+			printMessage("Multiple input operations found");
 		}
 
-		this.op = newOp;
+		this.inputOp = newOp;
+	}
+
+	private void setOutputOperation(OutputOperation newOp) {
+		if (outputOp != null) {
+			printMessage("Multiple output operations found");
+		}
+
+		this.outputOp = newOp;
 	}
 
 	private void run() throws FileNotFoundException, SQLException, ClassNotFoundException, ParserException {
 		// setup parser
 		Parser parser = null;
 
-		switch(op) {
+		switch(inputOp) {
 			case DELIMITED:
 				if (file != null) {
-					parser = new DelimitedParser(new BufferedReader(new InputStreamReader(new FileInputStream(new File(file)))), ',', true);
+					parser = new DelimitedParser(new BufferedReader(new InputStreamReader(new FileInputStream(new File(file)))), delimiter, textQuantifier, hasHeaderRow);
 				} else {
-					parser = new DelimitedParser(new BufferedReader(new InputStreamReader(System.in)), ',', true);
+					parser = new DelimitedParser(new BufferedReader(new InputStreamReader(System.in)), delimiter, textQuantifier, hasHeaderRow);
 				}
 			break;
 			case JDBC:
@@ -127,31 +179,52 @@ public class SQLConverterCLI {
 				parser = new JDBCParser(con);
 			break;
 			default:
-				System.out.println("This option hasn't been defined!");
-				System.exit(1);
+				printMessage("This input option hasn't been defined!");
+				System.exit(0);
 			break;
 		}
+
 
 		Statement[] statements = parser.parse();
 
 		if (statements == null) {
-			System.out.println("Parser returned no results");
-			System.exit(1);
+			printMessage("Parser returned no results");
 		}
 
+
 		// setup producer
-		Producer producer = new PostgreSQLProducer();
-		//Producer producer = new AccessSQLProducer();
+		Producer producer = null;
+
+		switch(outputOp) {
+			case POSTGRESQL:
+				 producer = new PostgreSQLProducer();
+			break;
+			default:
+				printMessage("This output option hasn't been defined!");
+				System.exit(0);
+			break;
+		}
 
 		producer.produce(statements);
 	}
 
-	private void printUsage() {
-		//System.out.println("jsqlparser < -mysql | -postgres | -sqlserver > < -stdin | -file <filename> >");
-		System.out.println("jsqlparser -jdbc -driver <driver> -url <url> [-user <username>] [-pass <password>]");
-		System.out.println("jsqlparser -delim < -stdin | -file <filename> >"); // [-seperator <char>] [-quantifier <char>]
+	private void printMessage(String message) {
+		System.out.println(message);
 
 		System.exit(0);
+	}
+
+	private void printUsage() {
+		System.out.println(
+			"jsqlparser <input options> <output options>\n" +
+
+			"input options:\n" +
+			"\t-jdbc -driver <driver> -url <url> [-user <username>] [-pass <password>]\n" +
+			"\t-delim [ -file <filename> ] [-noheader] [-seperator <char>] [-quantifier <char>]\n" +
+			"\n" +
+			"output options:\n" +
+			"\t-out-postgre"
+		);
 	}
 
 	public static void main (String[] args) throws ParserException, SQLException, FileNotFoundException, ClassNotFoundException {
