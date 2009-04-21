cc33526a193ec60c0d469c49ea8aa2eccbc5075d
jamescarter <jamescarter@83d163da-d654-0410-8eb7-0be868dce84a>
2009-04-21 21:59:31 +0000

    Added new WiP XMLParser that uses XPath which will replace the existing XMLParser(s) Added new MySQL Workbench XMLParser
    git-svn-id: https://jsqlconverter.googlecode.com/svn/trunk@50 83d163da-d654-0410-8eb7-0be868dce84a

---
+++ src/com/googlecode/jsqlconverter/parser/xml/SQLFairyXMLParser.java
@@ -0,0 +1,120 @@
+package com.googlecode.jsqlconverter.parser.xml;
+
+import org.w3c.dom.Element;
+import com.googlecode.jsqlconverter.definition.type.*;
+
+import java.io.InputStream;
+
+public class SQLFairyXMLParser extends XMLParser {
+	private String tagPrefix = null;
+
+	public SQLFairyXMLParser(InputStream in) {
+		super(in);
+	}
+
+	public SQLFairyXMLParser(InputStream in, String tagPrefix) {
+		super(in);
+
+		this.tagPrefix = tagPrefix;
+	}
+
+	public String getTableListPath() {
+		return "//table";
+	}
+
+	public String getTableNamePath() {
+		return "@name";
+	}
+
+	public String getColumnListPath(Element tableNode) {
+		return "fields/field";
+	}
+
+	public String getColumnNamePath() {
+		return getTableNamePath();
+	}
+
+	public String getPrimaryKeyPath() {
+		return "@is_primary_key='1'";
+	}
+
+	public String getAutoIncrementPath() {
+		return "@is_auto_increment='1'";
+	}
+
+	public String getIsRequiredPath() {
+		return "@is_nullable='0'";
+	}
+
+	public String getColumnSizePath() {
+		return "@size";
+	}
+
+	public String getDefaultValuePath() {
+		return "defaultValue";
+	}
+
+	public String getDataTypePath() {
+		return "@data_type";
+	}
+
+	public Type getType(String type, int size) {
+		type = type.toLowerCase();
+
+		if (type.equals("bit")) {
+			return BinaryType.BIT;
+		} else if (type.equals("tinyint")) {
+			return ExactNumericType.TINYINT;
+		} else if (type.equals("smallint")) {
+			return ExactNumericType.SMALLINT;
+		} else if (type.equals("mediumint")) {
+			return ExactNumericType.MEDIUMINT;
+		} else if (type.equals("int")) {
+			return ExactNumericType.INTEGER;
+		} else if (type.equals("bigint")) {
+			return ExactNumericType.BIGINT;
+		} else if (type.equals("float")) {
+			return ApproximateNumericType.FLOAT;
+		} else if (type.equals("double")) {
+			return ApproximateNumericType.DOUBLE;
+		} else if (type.equals("decimal") || type.equals("dec") || type.equals("numeric") || type.equals("fixed")) {
+			return new DecimalType(size);
+		} else if (type.equals("real")) {
+			return ApproximateNumericType.REAL;
+		} else if (type.equals("tinytext")) {
+			return StringType.TINYTEXT;
+		} else if (type.equals("tinyblob")) {
+			return BinaryType.TINYBLOB;
+		} else if (type.equals("blob")) {
+			return BinaryType.BLOB;
+		} else if (type.equals("text")) {
+			return StringType.TEXT;
+		} else if (type.equals("mediumblob")) {
+			return BinaryType.MEDIUMBLOB;
+		} else if (type.equals("mediumtext")) {
+			return StringType.MEDIUMTEXT;
+		} else if (type.equals("longblob")) {
+			return BinaryType.LONGBLOB;
+		} else if (type.equals("longtext")) {
+			return StringType.LONGTEXT;
+		} else if (type.equals("date")) {
+			return DateTimeType.DATE;
+		} else if (type.equals("datetime")) {
+			return DateTimeType.DATETIME;
+		} else if (type.equals("timestamp")) {
+			return DateTimeType.TIMESTAMP;
+		} else if (type.equals("time")) {
+			return DateTimeType.TIME;
+		} else if (type.equals("char")) {
+			return StringType.CHAR;
+		} else if (type.equals("varchar")) {
+			return StringType.VARCHAR;
+		} else if (type.equals("binary")) {
+			return BinaryType.BINARY;
+		} else if (type.equals("varbinary")) {
+			return BinaryType.VARBINARY;
+		}
+
+		return StringType.VARCHAR;
+	}
+}
---
+++ src/com/googlecode/jsqlconverter/parser/xml/XMLParser.java
@@ -0,0 +1,160 @@
+package com.googlecode.jsqlconverter.parser.xml;
+
+import com.googlecode.jsqlconverter.definition.Name;
+import com.googlecode.jsqlconverter.definition.create.table.Column;
+import com.googlecode.jsqlconverter.definition.create.table.ColumnOption;
+import com.googlecode.jsqlconverter.definition.create.table.CreateTable;
+import com.googlecode.jsqlconverter.definition.create.table.constraint.DefaultConstraint;
+import com.googlecode.jsqlconverter.definition.type.Type;
+import com.googlecode.jsqlconverter.parser.Parser;
+import com.googlecode.jsqlconverter.parser.ParserException;
+import com.googlecode.jsqlconverter.parser.callback.ParserCallback;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipFile;
+
+public abstract class XMLParser extends Parser {
+	protected InputStream in;
+	private XPath xpath;
+
+	public XMLParser(InputStream in) {
+		this.in = in;
+	}
+
+	public XMLParser(File zipFile, String xmlFileName) throws IOException {
+		ZipFile zip = new ZipFile(zipFile, ZipFile.OPEN_READ);
+
+		in = zip.getInputStream(zip.getEntry(xmlFileName));
+	}
+
+	public void parse(ParserCallback callback) throws ParserException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder;
+
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new ParserException(e.getMessage(), e.getCause());
+		}
+
+		Document document;
+
+		try {
+			document = builder.parse(in);
+		} catch (Exception e) {
+			throw new ParserException(e.getMessage(), e.getCause());
+		}
+
+		xpath = XPathFactory.newInstance().newXPath();
+
+		// TODO: support several DB (schema)
+
+		try {
+			NodeList nodes = (NodeList) xpath.compile(getTableListPath()).evaluate(document, XPathConstants.NODESET);
+
+			doTables(callback, nodes);
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void doTables(ParserCallback callback, NodeList tableNodes) throws XPathExpressionException {
+		for (int i = 0; i < tableNodes.getLength(); i++) {
+			doTable(callback, (Element)tableNodes.item(i));
+		}
+	}
+
+	private void doTable(ParserCallback callback, Element tableNode) throws XPathExpressionException {
+		String tableName = (String) xpath.evaluate(getTableNamePath(), tableNode, XPathConstants.STRING);
+
+		CreateTable ct = new CreateTable(new Name(tableName));
+
+		NodeList columnNodes = (NodeList) xpath.compile(getColumnListPath(tableNode)).evaluate(tableNode, XPathConstants.NODESET);
+
+		//NodeList indexNodes = table.getElementsByTagName("index");
+
+		for (int i=0; i<columnNodes.getLength(); i++) {
+			Column column = getColumn((Element)columnNodes.item(i));
+
+			ct.addColumn(column);
+		}
+
+		// TODO detect foreign-keys (turbine only)
+
+		// TODO detect unique columns
+
+		// TODO find indices
+
+		// send create table statement to be produced
+		callback.produceStatement(ct);
+
+		//System.exit(0);
+
+		// TODO detect non-unique table indexes
+	}
+
+	private Column getColumn(Element columnElement) throws XPathExpressionException {
+		String columnName = (String) xpath.evaluate(getColumnNamePath(), columnElement, XPathConstants.STRING);
+		boolean isPrimaryKey = Boolean.parseBoolean((String) xpath.evaluate(getPrimaryKeyPath(), columnElement, XPathConstants.STRING));
+		boolean isAutoIncr = Boolean.parseBoolean((String) xpath.evaluate(getAutoIncrementPath(), columnElement, XPathConstants.STRING));
+		boolean isRequired = Boolean.parseBoolean((String) xpath.evaluate(getIsRequiredPath(), columnElement, XPathConstants.STRING));
+		String colSize = (String) xpath.evaluate(getColumnSizePath(), columnElement, XPathConstants.STRING);
+		String defaultValue = (String) xpath.evaluate(getDefaultValuePath(), columnElement, XPathConstants.STRING);
+		String colType = (String) xpath.evaluate(getDataTypePath(), columnElement, XPathConstants.STRING);
+
+		int size = 0;
+
+		if (!colSize.isEmpty()) {
+			size = Integer.parseInt(colSize);
+		}
+
+		Column column = new Column(new Name(columnName), getType(colType, size));
+		column.setSize(size);
+
+		if (isPrimaryKey) {
+			column.addColumnOption(ColumnOption.PRIMARY_KEY);
+		}
+
+		if (isAutoIncr) {
+			column.addColumnOption(ColumnOption.AUTO_INCREMENT);
+		}
+
+		if (isRequired) {
+			column.addColumnOption(ColumnOption.NOT_NULL);
+		}
+
+		if (!defaultValue.isEmpty()) {
+			column.setDefaultConstraint(new DefaultConstraint(defaultValue));
+		}
+
+		return column;
+	}
+
+	public abstract String getTableListPath();
+
+	public abstract String getTableNamePath();
+
+	public abstract String getColumnListPath(Element tableNode);
+	public abstract String getColumnNamePath();
+	public abstract String getPrimaryKeyPath();
+	public abstract String getAutoIncrementPath();
+	public abstract String getIsRequiredPath();
+	public abstract String getColumnSizePath();
+	public abstract String getDefaultValuePath();
+
+	public abstract String getDataTypePath();
+
+	public abstract Type getType(String type, int size);
+}
